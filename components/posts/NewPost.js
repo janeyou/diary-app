aa66043bc0efc841e8fcde767b22f4a31d3897c2
Raeya Yotta <raeyayotta@gmail.com>
2019-07-26 16:41:43 -0700

    New  post form and mutation

--- components/posts/NewPost.js
+++ components/posts/NewPost.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
-export default class NewPost extends Component {
+import PostForm from './PostForm';
+
+class NewPost extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
 
+  newPost = ({ title, body }) => {
+    this.props
+      .newPost({
+        variables: {
+          title,
+          body
+        }
+      })
+      .then(() => {})
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   render() {
     return (
       <View>
-        <Text> NewPost </Text>
+        <PostForm onSubmit={this.newPost} />
       </View>
     );
   }
 }
+
+const newPost = gql`
+  mutation newPost($title: String!, $body: String) {
+    createPost(title: $title, body: $body) {
+      id
+    }
+  }
+`;
+
+export default graphql(newPost, {
+  name: 'newPost'
+})(NewPost);
---
+++ components/posts/PostForm.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, TextInput, Button } from 'react-native';
+
+export default class PostForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      body: ''
+    };
+  }
+
+  submitForm = () => {
+    this.props.onSubmit({
+      title: this.state.title,
+      body: this.state.body
+    });
+  };
+
+  render() {
+    return (
+      <View>
+        <TextInput
+          style={styles.title}
+          onChangeText={title => this.setState({ title })}
+          value={this.state.title}
+        />
+        <TextInput
+          style={styles.body}
+          onChangeText={body => this.setState({ body })}
+          value={this.state.body}
+        />
+        <Button title="Save Post" onPress={this.submitForm} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  title: {
+    height: 40,
+    borderColor: '#333',
+    borderWidth: 1
+  },
+  body: {
+    height: 400,
+    borderColor: '#333',
+    borderWidth: 1,
+    textAlignVertical: 'top'
+  }
+});
