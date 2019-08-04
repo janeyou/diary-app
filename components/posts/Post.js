018faea30d460305b3eafc7879e2585756600883
Jane You <jane2745gmail.com>
2019-08-03 17:50:45 -0700

    Post page UI

--- components/posts/Post.js
+++ components/posts/Post.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import {
-  View,
-  Text,
   ActivityIndicator,
-  StyleSheet,
-  Button
+  Button,
+  TouchableOpacity,
+  StatusBar
 } from 'react-native';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
-import { Fab, Icon } from 'native-base';
+import styled from 'styled-components';
+import moment from 'moment';
+import * as Icon from '@expo/vector-icons';
 
 import navStyles from '../../styles/navigationStyles';
 
@@ -29,40 +30,58 @@ class Post extends Component {
     });
   };
 
+  componentDidMount() {
+    StatusBar.setBarStyle('light-content', true);
+  }
+
+  componentWillUnmount() {
+    StatusBar.setBarStyle('dark-content', true);
+  }
+
   render() {
     const { loading, Post } = this.props;
     if (loading) return <ActivityIndicator size="large" />;
     return (
-      <View style={styles.container}>
-        <Text style={styles.bodyText}>{Post.body}</Text>
-        <Button
-          title="Close"
+      <Container>
+        <StatusBar hidden />
+        <Cover>
+          <Image source={require('../../assets/background2.jpg')} />
+          <Title>{Post.title}</Title>
+          <Time>{moment(new Date(Post.createdAt)).fromNow()}</Time>
+        </Cover>
+        <TouchableOpacity
           onPress={() => {
             this.props.navigation.goBack();
           }}
-        />
-        <Fab style={styles.editPost} onPress={this.editPost}>
-          <Icon name="create" />
-        </Fab>
-      </View>
+          style={{ position: 'absolute', top: 20, right: 20 }}
+        >
+          <CloseView>
+            <Icon.Ionicons
+              name="ios-close"
+              size={36}
+              color="#4775f2"
+              style={{ marginTop: -2 }}
+            />
+          </CloseView>
+        </TouchableOpacity>
+        <Button title="Close" />
+
+        <TouchableOpacity
+          onPress={this.editPost}
+          style={{
+            position: 'absolute',
+            bottom: 10,
+            right: 10
+          }}
+        >
+          <CreateBtn>
+            <Icon.Ionicons name="ios-create" size={44} color="white" />
+          </CreateBtn>
+        </TouchableOpacity>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 20,
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  editPost: {
-    backgroundColor: '#82D8D8'
-  },
-  bodyText: {
-    fontSize: 16
-  }
-});
 
 const postQuery = gql`
   query Post($id: ID!) {
@@ -70,6 +89,7 @@ const postQuery = gql`
       id
       title
       body
+      createdAt
     }
   }
 `;
@@ -82,3 +102,53 @@ export default graphql(postQuery, {
     }
   })
 })(Post);
+
+const Container = styled.View`
+  flex: 1;
+`;
+
+const Cover = styled.View`
+  height: 375px;
+`;
+const Image = styled.Image`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+`;
+const Title = styled.Text`
+  font-size: 24px;
+  color: white;
+  font-weight: bold;
+  width: 170px;
+  position: absolute;
+  top: 78px;
+  left: 20px;
+`;
+const Time = styled.Text`
+  color: white;
+  font-size: 17px;
+  position: absolute;
+  bottom: 20px;
+  left: 20px;
+  width: 300px;
+`;
+
+const CloseView = styled.View`
+  width: 32px;
+  height: 32px;
+  background: white;
+  border-radius: 16px;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.15);
+`;
+
+const CreateBtn = styled.View`
+  width: 44px;
+  height: 44px;
+  border-radius: 22px;
+  background: #82d8d8;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.15);
+`;
